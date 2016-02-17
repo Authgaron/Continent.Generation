cc222426486e2c49e9871a9755ed81e04be7a2e0
kevin.gray <kevin.gray@entelect.co.za>
2016-02-17 14:44:43 +0200

    Create database query drops tables, then recreates the new tables.

--- ContinentGenerator/ContinentGenerator/CreateDatabase.sql
+++ ContinentGenerator/ContinentGenerator/CreateDatabase.sql
@@ -1,9 +1,20 @@
-create table x(
-	ID bigint not null identity(1,1),
+if object_id('dbo.HeightMap', 'U') is not null
+	drop table dbo.HeightMap;
+if object_id('dbo.WorldNames', 'U') is not null
+	drop table dbo.WorldNames;
+
+create table dbo.WorldNames(
+	Seed int not null,
+	Name varchar(35),
+	primary key(Seed)
+);
+
+create table HeightMap(
 	Seed int not null,
 	X int not null,
 	Y int not null,
 	Scale smallint not null,
-	Height float not null
-	primary key(ID)
-)
+	Height float not null,
+	primary key(Seed, Scale, X, Y),
+	foreign key(Seed) references WorldNames(Seed)
+);
